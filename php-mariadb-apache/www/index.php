5c15d8738000d53b0d602d45b6ff8ce1c878e820
Harald Kirkerød <hkirkerod@oncoimmunity.com>
2017-06-14 14:07:35 +0200

    test mysql init

--- php-mariadb-apache/www/index.php
+++ php-mariadb-apache/www/index.php
@@ -1,1 +1,16 @@
-<?php phpinfo(); ?>
+<?php 
+    phpinfo(); 
+
+    $DBSERVER = "localhost"; 
+	$DBNAME = "database";
+	$DBUSER = "root";
+	$DBPASS = "test";
+
+    $LINK = mysqli_connect($DBSERVER, $DBUSER, $DBPASS);
+	mysqli_select_db($LINK, $DBNAME);
+
+    $row = mysqli_query("select * `test`")->fetch_assoc();
+
+    echo $row["greeting"];
+
+?>
